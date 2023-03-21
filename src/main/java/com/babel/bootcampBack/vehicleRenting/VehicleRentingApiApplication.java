dd45ca298468cd715241a9f59c063eac9af88e4a
andresguijarro-babel <andres.guijarro@babelgroup.com>
2023-03-21 09:47:46 +0100

    commit inicial

---
+++ src/main/java/com/babel/bootcampBack/vehicleRenting/VehicleRentingApiApplication.java
@@ -0,0 +1,13 @@
+package com.babel.bootcampBack.vehicleRenting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRentingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRentingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/babel/bootcampBack/vehicleRenting/VehicleRentingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babel.bootcampBack.vehicleRenting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRentingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
